1f9fd91ba92c71e5880236cad82d2539ab4672d9
kopo16 <skj7180@naver.com>
2021-04-15 16:57:23 +0900

    놀이공원(실수로 날려버려서 다시 올림)

---
+++ ParkTicket(2021.04.14)/ParkConstvalue.java
@@ -0,0 +1,24 @@
+package ParkTicket;
+
+public class ParkConstvalue {
+	public static final int Adult_DP = 56000;
+	public static final int Adult_NP = 46000;
+	public static final int Teen_DP = 47000;
+	public static final int Teen_NP = 40000;
+	public static final int Kid_DP = 44000;
+	public static final int Kid_NP = 37000;
+	public static final int Old_DP = 44000;
+	public static final int Old_NP = 37000;
+	public static final int Min_Baby = 1;
+	public static final int Min_Kid = 3;
+	public static final int Max_Kid = 12;
+	public static final int Min_Teen = 13;
+	public static final int Max_Teen = 18;
+	public static final int Min_Adult = 19;
+	public static final int Max_Adult = 64;
+	
+	
+	
+	
+
+}
